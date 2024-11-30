4e2bf673dc73dea876df6109edc78e260b3522bb
Sculkinn <158416970+Sculkinn@users.noreply.github.com>
2024-11-30 14:48:40 +1100

    Add project files.

---
+++ Hover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace LiftlandsModManager
+{
+    public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Button button;
+        private TextMeshProUGUI buttonText;
+        private Color originalColor;
+        private Color hoverColor = Color.gray;
+
+        void Start()
+        {
+            button = GetComponent<Button>();
+            buttonText = GetComponentInChildren<TextMeshProUGUI>();
+            originalColor = buttonText.color;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            buttonText.color = hoverColor;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            buttonText.color = originalColor;
+        }
+    }
+
+}
---
+++ LiftlandsModManager.cs
@@ -0,0 +1,82 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Diagnostics;
+using System.IO;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace LiftlandsModManager
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class LiftlandsModManager : BaseUnityPlugin
+    {
+        private const string MyGUID = "in.sculk.LiftlandsModManager";
+        private const string PluginName = "LiftlandsModManager";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private ConfigEntry<string> configString;
+        private ConfigEntry<bool> configBool;
+        private ConfigEntry<int> configInt;
+        private ConfigEntry<float> configFloat;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"{PluginName} {VersionString} is loading...");
+            Harmony.PatchAll();
+            Logger.LogInfo($"{PluginName} {VersionString} is loaded.");
+            Log = Logger;
+        }
+        private void Start()
+        {
+            GameObject menuCanvas = GameObject.Find("MainMenuManager/Canvas");
+            Transform menuButtons = menuCanvas.transform.Find("MenuButtons");
+            Vector3 newPosition = menuButtons.localPosition;
+            newPosition.y += 150;
+            menuButtons.localPosition = newPosition;
+
+            GameObject exitBtn = menuButtons.Find("EXIT")?.gameObject;
+
+            GameObject modsButton = new GameObject("Mods");
+            modsButton.name = "Mods";
+            modsButton.transform.SetParent(menuButtons);
+
+            Button buttonComponent = modsButton.AddComponent<Button>();
+
+            TextMeshProUGUI exitBtnText = exitBtn.GetComponentInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI modsBtnText = modsButton.AddComponent<TextMeshProUGUI>();
+
+            // Looks weird on non-1080p screens
+
+            modsBtnText.text = "MODS";
+            modsBtnText.fontSize = exitBtnText.fontSize;
+            modsBtnText.font = exitBtnText.font;
+            modsBtnText.alignment = exitBtnText.alignment;
+            modsBtnText.outlineColor = exitBtnText.outlineColor;
+            modsBtnText.outlineWidth = exitBtnText.outlineWidth;
+            modsBtnText.fontStyle = FontStyles.Bold;
+
+            RectTransform modsBtnRect = modsButton.GetComponent<RectTransform>();
+            RectTransform exitBtnRect = exitBtn.GetComponent<RectTransform>();
+
+            modsBtnRect.localScale = exitBtnRect.localScale;
+
+            modsButton.transform.SetSiblingIndex(3);
+
+            modsButton.AddComponent<Hover>();
+
+            buttonComponent.interactable = true;
+            buttonComponent.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            GameObject.Find("MainMenuManager").SetActive(false);
+        }
+    }
+}
